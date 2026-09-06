f110bec6c60a8e1a39fd719f2acc2725db739bda
OppaAI <oppa.ai.org@proton.me>
2026-09-06 11:40:17 -0700

    chore(gradle): Remove redundant Kotlin Android plugin
    - Remove `org.jetbrains.kotlin.android` plugin from app build.gradle.kts
    - Remove `org.jetbrains.kotlin.android` plugin from root build.gradle.kts
    - Add Aiko Lingo app icon (aiko_square.png) to drawable resources
    - Compose plugin handles Kotlin configuration, eliminating the need for separate Android plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ import org.gradle.api.initialization.resolve.RepositoriesMode
 
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
